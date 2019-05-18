cc5174cf5b35f2565aadad8a1266c4e3581f0d7e
Sun & Baraja <bodofocus14@gmail.com>
2019-05-18 10:25:05 +0530

    before install prompt added

--- sw.js
+++ sw.js
@@ -9,3 +9,14 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
     console.log('service worker has been activated')
 })
+
+//before install prompt
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
